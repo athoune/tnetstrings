29c5dd926bf9c13ba293f5ca63e0193d76976402
Mathieu Lecarme <mathieu@garambrogne.net>
2011-11-04 23:26:54 +0100

    encode string, boolean, float, list.

--- src/tnetstrings.erl
+++ src/tnetstrings.erl
@@ -9,9 +9,23 @@
 my_func() ->
     ok.
 
+encode(B) when B == true -> "4:true!";
+encode(false) -> "5:false!";
+encode(F) when is_float(F) ->
+    [A] = io_lib:format("~w", [F]),
+    with_size(A) ++ "^";
 encode(N) when is_number(N) ->
     [A] = io_lib:format("~w", [N]),
-    with_size(A) ++ "#".
+    with_size(A) ++ "#";
+encode(S) when is_binary(S) ->
+    with_size(binary_to_list(S)) ++ ",";
+encode(A) when is_atom(A) ->
+    with_size(atom_to_list(A)) ++ ",";
+encode(L) when is_list(L) ->
+    with_size(lists:foldl(
+        fun(I, Acc) ->
+            Acc ++ encode(I)
+        end, [], L)) ++ "]".
 
 with_size(A) ->
     [S] = io_lib:format("~w", [iolist_size(A)]),
--- test/tnetstrings_tests.erl
+++ test/tnetstrings_tests.erl
@@ -1,5 +1,16 @@
 -module(tnetstrings_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-int_test() ->
-    ?assertEqual("2:42#", tnetstrings:encode(42)).
+number_test() ->
+    ?assertEqual("2:42#", tnetstrings:encode(42)),
+    ?assertEqual("8:3.141509^", tnetstrings:encode(3.141509)).
+
+string_test() ->
+    ?assertEqual("5:Beuha,", tnetstrings:encode('Beuha')),
+    ?assertEqual("5:Beuha,", tnetstrings:encode(<<"Beuha">>)).
+
+boolean_test() ->
+    ?assertEqual("4:true!", tnetstrings:encode(true)).
+
+list_test() ->
+    ?assertEqual("15:1:1#1:a,4:true!]", tnetstrings:encode([1, <<"a">>, true])).
